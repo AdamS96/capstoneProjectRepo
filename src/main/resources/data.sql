1a8ae8804fdd46453c5de70a21a34243ab085b20
Adam Stewart <adamstewart94@gmail.com>
2021-11-04 16:47:32 +0000

    added another driver to sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,14 @@
-    INSERT INTO DRIVER_DETAILS(id, prefix, first_name, last_name, contact_number, address_line1, address_line2, city,
+INSERT INTO DRIVER_DETAILS(id, prefix, first_name, last_name, contact_number, address_line1, address_line2, city,
                       postcode,
                       vehicle_type, engine_size, additional_drivers, used_commercial_purposes, used_outside_state,
                       current_vehicle_value,
                       date_vehicle_registered, quote_amount)
 VALUES (1, 'Mr', 'John', 'Healy', '07812345678', '123 Allstate Lane', 'Apartment 1', 'Belfast', 'BT1 1AB', 'Coupe',
         '2000', '1', 'Yes', 'Yes', '25000', '01-01-2001', 232.32);
+INSERT INTO DRIVER_DETAILS(id, prefix, first_name, last_name, contact_number, address_line1, address_line2, city,
+                           postcode,
+                           vehicle_type, engine_size, additional_drivers, used_commercial_purposes, used_outside_state,
+                           current_vehicle_value,
+                           date_vehicle_registered, quote_amount)
+VALUES (2, 'Mrs', 'Janice', 'Jones', '07862726155', '123 Belview Street', 'Apartment 1', 'Newtownards', 'BT21 7YT', 'Hatchback',
+        '3000', '2', 'No', 'No', '15000', '07-05-2015', 633.60);
