414cb759d93fb5126c65e090f58eec139347041c
Adam Stewart <adamstewart94@gmail.com>
2021-11-25 17:30:34 +0000

    - Added DisplayDrivers in Service - Added ReadAllDrivers in frontend

---
+++ frontend/src/components/ReadAllDrivers/ReadAllDrivers.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './ReadAllDrivers.css';
+import {Button, Form, Row, Col, Container, Alert, Table} from 'react-bootstrap';
+import SERVER_URL from "../../utils/constans";
+import {useHistory} from "react-router";
+
+
+function ReadAllDrivers(){
+
+    const [tableData, setTableData] = useState([]);
+    let history = useHistory();
+    function callMockApiWithAxiosGET() {
+
+        const endpointURL = `${SERVER_URL}/drivers/`;
+        axios.get(endpointURL)
+            .then(response => setTableData(response.data))
+            .catch(
+                (err) => { console.log(err) }
+            );
+    }
+
+    useEffect(() => {
+        callMockApiWithAxiosGET();
+    }, []);
+
+    return(
+        <main className="page readAllDrivers-page">
+            <section className="readAllDrivers-page dark">
+                <Container>
+                    <div className="block-heading">
+                        <h2>All Driver Records</h2>
+                        <p></p>
+                    </div>
+                    <Row>
+                        <div className="readAllDrivers-panel">
+                            <Row>
+                                <Col xs={12}>
+                                    <section className="readAllDrivers-panel-layout">
+
+                                            <h3 className="section-heading">Personal Details For Quoted Drivers</h3>
+                                            <Table striped bordered hover size="sm">
+                                                <thead>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>First Name</th>
+                                                    <th>Last Name</th>
+                                                    <th>Contact Number</th>
+                                                    <th>Address Line 1</th>
+                                                    <th>Address Line 2</th>
+                                                    <th>City</th>
+                                                    <th>Postcode</th>
+
+                                                    <th>View Driver</th>
+                                                </tr>
+                                                </thead>
+                                                <tbody>
+                                                {tableData.map(
+                                                    data => {
+
+                                                        return(
+                                                            <tr>
+                                                                <td>{data.id}</td>
+                                                                <td>{data.firstName}</td>
+                                                                <td>{data.lastName}</td>
+                                                                <td>{data.contactNumber}</td>
+                                                                <td>{data.addressLine1}</td>
+                                                                <td>{data.addressLine2}</td>
+                                                                <td>{data.city}</td>
+                                                                <td>{data.postcode}</td>
+
+                                                                <td>
+                                                                    <Button type="submit" onClick={() => {
+                                                                        history.push(`../readdriver/${data.id}`)
+                                                                    }}>Details</Button>
+                                                                </td>
+                                                            </tr>
+                                                        )})}
+                                                </tbody>
+                                            </Table>
+                                        <h3 className="section-heading"></h3>
+                                        <Link to="/administrator">
+                                            <Button type="submit">Admin Page</Button>
+                                        </Link>
+                                    </section>
+                                </Col>
+                            </Row>
+                        </div>
+                    </Row>
+                </Container>
+            </section>
+        </main>
+    )
+}
+
+export default ReadAllDrivers;
---
+++ src/main/java/com/example/drivers/service/businessLogic/DisplayDrivers.java
@@ -0,0 +1,32 @@
+package com.example.drivers.service.businessLogic;
+
+import com.example.drivers.model.Driver;
+
+import java.util.List;
+
+public class DisplayDrivers {
+
+    public static void displayDrivers(List<Driver> driverList){
+        System.out.printf("%n%-10.10s %-15.15s %-15.15s %-20.20s %-25.25s %-25.25s %-20.20s %-15.15s %-15.15s %-15.15s %-20.20s %-15.15s %-25.25s %-15.15s %-20.20s %-20.20s %n",
+                "Prefix", "First Name", "Last Name", "Telephone Number", "Address Line 1", "Address Line 2",
+                "City", "Postcode", "Vehicle Type", "Engine Size", "Additional Drivers", "Commercial Use",
+                "Outside of State Use", "Current Value", "Date Registered", "Final Quote Amount");
+
+        for(Driver driver : driverList){
+
+//        System.out.println(
+//                driver.getPrefix() + driver.getFirstName() + driver.getLastName() + driver.getContactNumber() +
+//                driver.getAddressLine1() + driver.getAddressLine2() + driver.getCity() + driver.getPostcode() +
+//                driver.getVehicleType() + driver.getEngineSize() + driver.getAdditionalDrivers() +
+//                driver.getUsedCommercialPurposes() + driver.getUsedOutsideState() + driver.getCurrentVehicleValue() +
+//                driver.getDateVehicleRegistered() + driver.getQuoteAmount());
+//    }
+    System.out.printf("%-10.10s %-15.15s %-15.15s %-20.20s %-25.25s %-25.25s %-20.20s %-15.15s %-15.15s %-15.15s %-20.20s %-15.15s %-25.25s %-15.15s %-20.20s %.2f %n",
+                driver.getPrefix(), driver.getFirstName(), driver.getLastName(), driver.getContactNumber(),
+                driver.getAddressLine1(), driver.getAddressLine2(), driver.getCity(), driver.getPostcode(),
+                driver.getVehicleType(), driver.getEngineSize(), driver.getAdditionalDrivers(),
+                driver.getUsedCommercialPurposes(), driver.getUsedOutsideState(), driver.getCurrentVehicleValue(),
+                driver.getDateVehicleRegistered(), driver.getQuoteAmount());
+    }
+}
+}
