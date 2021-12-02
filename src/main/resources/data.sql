6253646adc84f9f58fa6503bed223963590be162
Adam Stewart <adamstewart94@gmail.com>
2021-12-02 09:44:00 +0000

    Refactored data sql file values

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO DRIVER_DETAILS(id, prefix, first_name, last_name, contact_number, ad
                       vehicle_type, engine_size, additional_drivers, used_commercial_purposes, used_outside_state,
                       current_vehicle_value,
                       date_vehicle_registered, quote_amount)
-VALUES (1, 'Mr', 'John', 'Healy', '07812345678', '123 Allstate Lane', 'Apartment 1', 'Belfast', 'BT1 1AB', 'Coupe',
+VALUES (1, 'Mr', 'David', 'Brown', '07816121274', '44 Allstate Drive', 'Apartment 1', 'Belfast', 'BT4 3FA', 'Coupe',
         '2000', '1', 'Yes', 'Yes', '25000', '01-01-2001', 232.32);
 INSERT INTO DRIVER_DETAILS(id, prefix, first_name, last_name, contact_number, address_line1, address_line2, city,
                            postcode,
